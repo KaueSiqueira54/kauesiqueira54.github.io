ca644c449bb2da6c75904bedc447a8bc79efd671
KaueSiqueira <kaue54150@gmail.com>
2024-12-14 23:44:02 -0300

    finalizando..

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,12 @@ function changeTheme() {
 
 toggleTheme.addEventListener("click", changeTheme);
 
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"))
+        item.classList.add("active");
+    })
+})
+
